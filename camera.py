7c86013f9ce653bf6e3feeea6110022813c28e3e
whleng <lengwenhui@gmail.com>
2021-11-06 18:04:57 -0400

    Added Gaussian blur to find brightest spot

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import cv2 as cv
+
 print( cv.__version__ )
 
 cap = cv.VideoCapture(0)
@@ -15,9 +16,17 @@ while True:
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
     # Converts the grayscale image to black and white
-    (thresh, blackAndWhiteImage) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+    # https://techtutorialsx.com/2019/04/13/python-opencv-converting-image-to-black-and-white/
+    #(thresh, blackAndWhiteImage) = cv.threshold(gray, 127, 255, cv.THRESH_BINARY)
+    #(minVal, maxVal, minLoc, maxLoc) = cv.minMaxLoc(gray)
+    #cv.circle(frame, maxLoc, 5, (255, 0, 0), 2)
+    
+    # apply a Gaussian blur to the image then find the brightest region
+    gray = cv.GaussianBlur(gray, (11,11), 0)
+    (minVal, maxVal, minLoc, maxLoc) = cv.minMaxLoc(gray)
+    cv.circle(frame, maxLoc, 10, (255, 0, 0), 2)
     # Display the resulting frame
-    cv.imshow('frame', blackAndWhiteImage)
+    cv.imshow('frame', frame)
     if cv.waitKey(1) == ord('q'):
         break
 # When everything done, release the capture
