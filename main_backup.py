6753a2cf0e6720241d3e615ba78483f616b18526
mingkaic1 <mingkaic@andrew.cmu.edu>
2021-11-06 22:23:37 -0400

    Smoother light tracking, FPSmeter

--- main_backup.py
+++ main_backup.py
@@ -1,42 +1,68 @@
 WIDTH = 1200
 HEIGHT = 800
-CURSOR_LIST_LENGTH = 10
-CURSOR_RA_NUM = 5
-CALIBRATION_RECTANGLE_TEMP = [0, 640, 0, 480] # TEMP
+CURSORLISTLENGTH = 10
+CURSORMAXVELOCITY = 50
+CURSORRANUM = 5
 
 from cmu_112_graphics import *
 import videoInput as vi
-import fpsmeter
 import cv2 as cv
 import statistics
+
+# Version 1: Use velocity and acceleration
+def updateCursor(app):
+    # Get data from videoInput
+    inputData = vi.getPoint(app.cap)
+    cursorNew = convertPoint(app, inputData[0], inputData[1])
+    if len(app.cursorQueue) == 0:
+        cursorPrev = (app.width // 2, app.height // 2)
+    else:
+        cursorPrev = app.cursorQueue[-1]
+    app.cursorAccel = (cursorNew[0] - cursorPrev[0], cursorNew[1] - cursorPrev[1])
+    if getMagnitude(*app.cursorAccel) < 200:
+        app.cursor = cursorNew
+    else:
+        app.cursorVelocity = (app.cursorVelocity[0] + app.cursorAccel[0],
+                              app.cursorVelocity[1] + app.cursorAccel[1])
+        constrainVelocity(app, CURSORMAXVELOCITY)
+        app.cursor = (cursorPrev[0] + app.cursorVelocity[0], cursorPrev[1] + app.cursorVelocity[1])
+    app.cursorQueue.append(app.cursor)
+    while len(app.cursorQueue) > CURSORLISTLENGTH:
+        app.cursorQueue.pop(0)
+    app.cursorCount += 1
 
 # Version 2: Use running average
 def updateCursor(app):
     # Get data from videoInput
     inputData = vi.getPoint(app.cap)
     cursorNew = convertPoint(app, inputData[0], inputData[1])
-    # app.cursurQueueRaw stores a queue of the CURSOR_RA_NUM most recent points
     app.cursorQueueRaw.append(cursorNew)
-    # Ensure that a max of CURSOR_RA_NUM points is queued in app.cursorQueueRaw
-    while len(app.cursorQueueRaw) > CURSOR_RA_NUM:
+    while len(app.cursorQueueRaw) > CURSORRANUM:
         app.cursorQueueRaw.pop(0)
-    # If app.cursorQueueRaw doesn't have enough points to average (at the beginning of app), use raw points
-    # Otherwise, average the CURSOR_RA_NUM most recent raw points, and append to app.cursorQueue
-    if len(app.cursorQueueRaw) < CURSOR_RA_NUM:
+    if len(app.cursorQueueRaw) < CURSORRANUM:
         app.cursorQueue.append(cursorNew)
     else:
         cursorRAx = statistics.mean([point[0] for point in app.cursorQueueRaw])
         cursorRAy = statistics.mean([point[1] for point in app.cursorQueueRaw])
         cursorRA = (cursorRAx, cursorRAy)
         app.cursorQueue.append(cursorRA)
-    # Ensure that a max of CURSOR_LIST_LENGTH points is queued in app.cursorQueue
-    while len(app.cursorQueue) > CURSOR_LIST_LENGTH:
+    while len(app.cursorQueue) > CURSORLISTLENGTH:
         app.cursorQueue.pop(0)
-    # Increment app.cursorCount (for debugging/reference purposes)
     app.cursorCount += 1
 
-# Helper function
+def getMagnitude(x, y):
+    return (x ** 2 + y ** 2) ** 0.5
+
+def constrainVelocity(app, maxSpeed):
+    vx, vy = app.cursorVelocity
+    speed = (vx ** 2 + vy ** 2) ** 0.5
+    if speed > maxSpeed:
+        vx = vx * (maxSpeed / speed)
+        vy = vy * (maxSpeed / speed)
+    app.cursorVelocity = (vx, vy)
+
 # Takes in OpenCV coordinates, and converts to app window coordinates based on app.calibrationRectangle
+calibrationRectangleTemp = [0, 640, 0, 480] # TEMP
 def convertPoint(app, inputX, inputY):
     x = (inputX - app.calibrationRectangle[0])/(app.calibrationRectangle[1] - app.calibrationRectangle[0]) * app.width
     y = (inputY - app.calibrationRectangle[2])/(app.calibrationRectangle[3] - app.calibrationRectangle[2]) * app.height
@@ -48,14 +74,15 @@ def convertPoint(app, inputX, inputY):
 
 def appStarted(app):
     app.mode = "calibrationMode"
-    app.calibrationRectangle = CALIBRATION_RECTANGLE_TEMP
+    app.calibrationRectangle = calibrationRectangleTemp
     app.cursor = (0, 0)
+    app.cursorVelocity = (0, 0)
+    app.cursorAccel = (0, 0)
     app.cursorQueue = []
-    app.cursorQueueRaw = []
     app.cursorCount = 0
     app.cap = cv.VideoCapture(0)
+    app.cursorQueueRaw = []
     app.timerDelay = 10
-    app.fpsmeter = fpsmeter.FPSmeter()
 
 # --------------------
 # CALIBRATION MODE
@@ -64,14 +91,13 @@ def appStarted(app):
 def calibrationMode_timerFired(app):
     updateCursor(app)
     print(app.cursorCount, app.cursorQueue)
-    app.fpsmeter.addFrame()
 
 def calibrationMode_redrawAll(app, canvas):
-    canvas.create_text(app.width//2, app.height//2, text = "Calibration Mode")
+    canvas.create_text(app.width//2, app.height//2, text = "calib mode")
     # canvas.create_oval(app.cursor[0] - 5, app.cursor[1] - 5, app.cursor[0] + 5, app.cursor[1] + 5, fill = "red")
     for i in range(len(app.cursorQueue) - 1):
         canvas.create_line(*app.cursorQueue[i], *app.cursorQueue[i + 1], width = 10)
-    canvas.create_text(app.width//2, app.height * 0.75, text = f"FPS: {round(app.fpsmeter.getFPS())}")
+    pass
 
 # --------------------
 # GAME MODE
@@ -81,7 +107,7 @@ def gameMode_timerFired(app):
     pass
 
 def gameMode_redrawAll(app, canvas):
-    canvas.create_text(app.width // 2, app.height // 2, text="Game Mode")
+    canvas.create_text(app.width // 2, app.height // 2, text="game mode")
     pass
 
 runApp(width = WIDTH, height = HEIGHT)
